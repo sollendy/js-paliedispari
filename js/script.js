0450f2e0d8f81d067c226023456b54dc38fb150f
sollendy <edlannino@tutanota.com>
2023-03-02 23:28:56 +0100

    problemi con renderizzazione degli esiti

--- js/script.js
+++ js/script.js
@@ -15,37 +15,36 @@ const btnPalEl = document.getElementById('test-btn');
 const esito = document.getElementById('campo-esito');
 
 //creo una variabile per la parola da analizzare
-let parola = CampoPaEl.value;
+let parola;
 
 //creo la funzione per la verifica della palindromia
 function testPalindromia() {
-
+    
     // find the length of a string
     const len = parola.length;
 
     // loop through half of the string
     for (let i = 0; i < len / 2; i++) {
-        
+
         // check if first and last string are same
         if (parola[i] !== parola[len - 1 - i]) {
             let newEl = document.createElement("b");
-            newEl.innerHTML = "non vi è palindromia";
+            newEl.innerText = "non vi è palindromia.";
             esito.append(newEl);
-            return 'non è palindroma';
         } else {
             let newEl = document.createElement("b");
-            newEl.innerHTML = "palindromia confermata";
+            newEl.innerText = "palindromia confermata!";
             esito.append(newEl);
-            return 'palindromia confermata';
         }
     }
 }
 
-console.log("fuori l'evento", testPalindromia);
+//console.log("fuori l'evento", testPalindromia);
 
 btnPalEl.addEventListener('click', function() {
-    value;
-    console.log("dentro l'evento", testPalindromia);
+    parola = CampoPaEl.value;
+    esito.innerHTML = testPalindromia(parola);
+    console.log("dentro l'evento", testPalindromia(parola));
 });
 
 /*
