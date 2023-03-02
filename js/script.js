a027780021e60c541ae0f8733f08daec75640d47
sollendy <edlannino@tutanota.com>
2023-03-02 15:58:57 +0100

    provata soluzione da stackoverflow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 //creo una variabile per il campo dell'input
 //creo una variabile per il bottone
-//
+//creo la funzione per la verifica della palindromia
+//creo l'evento per il click al bottone
+//inserisco la funzione nell'evento
+//nell'evento creo l'output per scrivere il risultato
+
+//creo una variabile per il campo dell'input
+const CampoPaEl = document.getElementById('campo-palindroma');
+
+//creo una variabile per il bottone
+const btnPalEl = document.getElementById('test-btn');
+
+const parola = CampoPaEl.value;
+
+//creo la funzione per la verifica della palindromia
+function testPalindromia(parola) {
+     // find the length of a string
+     const len = parola.length;
+
+     // loop through half of the string
+     for (let i = 0; i < len / 2; i++) {
+ 
+         // check if first and last string are same
+         if (parola[i] !== parola[len - 1 - i]) {
+             return 'non è palindroma';
+         }
+     }
+     return 'palindromia confermata';
+
+}
+
+console.log();
